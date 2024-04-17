0917e1d0d66d4eb87bba17cd21b16e3668833b2a
= <=>
2024-04-16 21:18:30 -0300

    first commit

--- manin.py
+++ manin.py
@@ -1,5 +1,6 @@
 #vou importar a classe Flask do pacote flask
-from flask import Flask
+#o Make_reponse geralmente gera em JSON
+from flask import Flask,make_response,jsonify,request
 from dados import Materias
 #FIXME: Flask e um classe
 #instãncianfo o objeto app 
@@ -9,24 +10,28 @@ app = Flask("__name__")
 #NOTE: vamos decorar
 #decorators
 #será que o flack consegue entender essa função solta ia?
-#POST(ins )
 
 
 @app.route('/materias', methods = ['GET'])
 def get_materias():
-    return Materias
+    return make_response    (   jsonify (   Materias    )      ) 
+
+
+@app.route("/")
+def index():
+    return "Projeto Integrador Base (Enem)"
 
 
 
 
-
-
-
-
-
+#request
+#POST
+@app.route('/materias',methods=['POST'])
+def create_materia():
+    Materia = request.json
+    return Materia
 
 
 
 #executar a API
 app.run()
-
